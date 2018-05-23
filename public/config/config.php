1a51a974e248b8403d74519a36fadea736e7de3d
Phil Gyford <phil@gyford.com>
2018-05-23 10:54:40 +0100

    Add config file

---
+++ public/config/config.php
@@ -0,0 +1,7 @@
+<?php
+
+$config['site_title'] = 'Well-Formed';
+
+$config['date_format'] = '%Y-%m-%d';            // Set the PHP date format as described here: http://php.net/manual/en/function.strftime.php
+
+$config['timezone'] = 'UTC';                 // Timezone may be required by your php install
