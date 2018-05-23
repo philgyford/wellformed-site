0837cbf6dd77315369dbad4151d52cdeb74f9552
Phil Gyford <phil@gyford.com>
2018-05-23 11:34:57 +0100

    Add CSS reboot to theme

--- public/config/config.php
+++ public/config/config.php
@@ -6,4 +6,4 @@ $config['date_format'] = '%Y-%m-%d';            // Set the PHP date format as de
 
 $config['timezone'] = 'UTC';                 // Timezone may be required by your php install
 
-$config['theme'] = 'well-formed-2018';                // Set the theme (defaults to "default")
+$config['theme'] = 'well-formed';                // Set the theme (defaults to "default")
